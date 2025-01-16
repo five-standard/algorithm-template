9868c00c83112e814a8b322161368b1c9c64c8f5
육기준 <dbrrl1127@gmail.com>
2025-01-16 20:48:37 +0900

    javascript 템플릿 추가

---
+++ template.js
@@ -0,0 +1,21 @@
+if ((path = process.platform !== "linux")) {
+  const start = new Date();
+  const memory = Math.floor(process.memoryUsage().heapUsed / 1024 / 1024);
+
+  console.log(solution(require("fs").readFileSync("in.txt").toString().trim()));
+  console.log(`memory: ${memory}mb`);
+  console.log(`time: ${new Date() - start}ms`);
+
+  return;
+}
+
+// --- 위 코드는 제출시 복사하지 말 것 ---
+
+console.log(solution(require("fs").readFileSync("/dev/stdin").toString().trim()));
+
+function solution(input) {
+  const [a, b] = input.split(" "); // input이 "a b일 경우"
+
+  return a + b;
+}
+
